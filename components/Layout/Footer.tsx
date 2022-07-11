8175a3768f4cedcfa9cd945b89bf618396123fd4
ThomasDos <dosanjos.thomas@gmail.com>
2022-07-11 14:56:18 +0200

    feat: 🎸 creation footer and ajout icons
    creation du footer selon maquette avec lib dsfr

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,15 +1,62 @@
-import { Footer, FooterBody, FooterBodyItem, Link } from "@dataesr/react-dsfr";
+import {
+    Footer,
+    FooterBody,
+    FooterBodyItem,
+    FooterBottom,
+    FooterCopy,
+    FooterLink,
+    FooterOperator,
+    Link,
+    Logo,
+} from "@dataesr/react-dsfr";
+import Image from "next/image";
 
 const FooterContainer = () => {
     return (
         <Footer>
-            <FooterBody description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Uenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.">
+            <FooterBody description="Texte optionnel 3 lignes maximum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur et vel quam auctor semper. Cras si amet mollis dolor.">
+                <Logo splitCharacter={10}>RÉPUBLIQUE FRANÇAISE</Logo>
+                <FooterOperator>
+                    <Image
+                        src="/icons/incubateur-territoires.svg"
+                        height="90px"
+                        width="160px"
+                        alt="logo incubateur territoires"
+                    />
+                </FooterOperator>
                 <FooterBodyItem>
-                    <Link href="https://service-public.fr">
+                    <Link href="https://legifrance.gouv.fr" className="mr-3">
+                        legifrance.gouv.fr
+                    </Link>
+                    <Link href="https://gouvernement.fr" className="mx-3">
+                        gouvernement.fr
+                    </Link>
+                    <Link href="https://service-public.fr" className="mx-3">
                         service-public.fr
+                    </Link>
+                    <Link href="https://data.gouv.fr" className="ml-3">
+                        data.gouv.fr
                     </Link>
                 </FooterBodyItem>
             </FooterBody>
+            <FooterBottom>
+                <FooterLink href="/">Plan du site</FooterLink>
+                <FooterLink href="/">Accessibilité</FooterLink>
+                <FooterLink href="/">Mentions légales</FooterLink>
+                <FooterLink href="/">Données personnelles</FooterLink>
+                <FooterLink href="/">Gestion des cookies</FooterLink>
+                <FooterLink href="/">
+                    <Image
+                        src="/icons/half-sun.svg"
+                        height="14.67px"
+                        width="14.67px"
+                        alt="icon paramètre d'affichage"
+                    />
+
+                    <span className="ml-2">Paramètres d&apos;affichage</span>
+                </FooterLink>
+                <FooterCopy>licence etalab-2.0</FooterCopy>
+            </FooterBottom>
         </Footer>
     );
 };
