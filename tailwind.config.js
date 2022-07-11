f608a85d38d87b3a6818b5ba2e1aa4ea49e7dbc3
ThomasDos <dosanjos.thomas@gmail.com>
2022-07-11 15:17:32 +0200

    refactor: 💡 update structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+    content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
     theme: {
         extend: {},
     },
