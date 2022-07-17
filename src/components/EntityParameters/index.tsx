a57824c6b0362de8f7e5a67aa50eaa7b88ecc7c0
ThomasDos <dosanjos.thomas@gmail.com>
2022-07-17 10:27:09 +0200

    feat: 🎸 component maindashboard layout, wip bodydashboard
    création du layout de la section principale du dashboard

---
+++ src/components/EntityParameters/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const EntityParametersContainer = styled.div`
+    width: 25%;
+    background: var(--grey-975-75);
+`;
+
+const EntityParameters = () => {
+    return (
+        <EntityParametersContainer>EntityParameters</EntityParametersContainer>
+    );
+};
+
+export default EntityParameters;
