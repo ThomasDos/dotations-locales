8131912d1a8718accc4265db50fe72266fe9f3ee
ThomasDos <dosanjos.thomas@gmail.com>
2022-07-17 11:18:37 +0200

    test: 💍 clean up and fix tests

--- src/utils/__tests__/formatNumberWithSpace.test.ts
+++ src/utils/__tests__/formatNumberWithSpace.test.ts
@@ -3,6 +3,7 @@ import formatNumberWithSpace from "../formatNumberWithSpace";
 describe("formatNumberWithSpace", () => {
     it("should return a number formatted with space every three digits", () => {
         const numberToFormat = 123456789;
+
         expect(formatNumberWithSpace(numberToFormat)).toEqual("123 456 789");
     });
 });
