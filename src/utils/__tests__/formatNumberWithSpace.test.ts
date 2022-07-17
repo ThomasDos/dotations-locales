f0cebf411aa90ad97e350feab44ec221baa9a88f
ThomasDos <dosanjos.thomas@gmail.com>
2022-07-17 10:42:33 +0200

    test: 💍 setup jest, add test for utils
    setup jest pour tout le projet, fix ts.config pour jest, ajout d'un test
    pour formatNumberWithSpace

--- src/utils/__tests__/formatNumberWithSpace.test.ts
+++ src/utils/__tests__/formatNumberWithSpace.test.ts
@@ -3,7 +3,6 @@ import formatNumberWithSpace from "../formatNumberWithSpace";
 describe("formatNumberWithSpace", () => {
     it("should return a number formatted with space every three digits", () => {
         const numberToFormat = 123456789;
-
         expect(formatNumberWithSpace(numberToFormat)).toEqual("123 456 789");
     });
 });
