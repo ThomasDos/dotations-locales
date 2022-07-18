5e9bfc3ea2ee04e2de21db176588eeffeed28e62
ThomasDos <dosanjos.thomas@gmail.com>
2022-07-18 08:47:05 +0200

    refactor: 💡 update folder structures

--- src/components/dashboard/DashboardBody/index.tsx
+++ src/components/dashboard/DashboardBody/index.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import styled from "styled-components";
 
-import DotationCard from "./DotationCard";
-
 const DashboardBodyContainer = styled.div`
     width: 75%;
     padding: 56px 80px 120px 120px;
@@ -20,7 +18,7 @@ const SpanMajContainer = styled.span`
 const DashboardBody = () => {
     return (
         <DashboardBodyContainer>
-            <InfoDateContainer className="px-8 py-4 mb-10 flex justify-between">
+            <InfoDateContainer className="px-8 py-4 flex justify-between">
                 <span className="text-base font-bold">
                     Vos dotations connues à ce jour pour l&apos;année 2022
                 </span>
@@ -28,7 +26,6 @@ const DashboardBody = () => {
                     <SpanMajContainer className="mr-1 text-sm">
                         Mise à jour hier à 08h45
                     </SpanMajContainer>
-
                     <Image
                         src="/icons/clock.svg"
                         height="16px"
@@ -38,11 +35,6 @@ const DashboardBody = () => {
                     />
                 </div>
             </InfoDateContainer>
-            <DotationCard
-                dotationTotal={870967}
-                title="Dotations Générales de Fonctionnement (DGC)"
-                description="Evolution de votre montant total de dotations"
-            />
         </DashboardBodyContainer>
     );
 };
--- src/components/dashboard/EntityParameters/index.tsx
+++ src/components/dashboard/EntityParameters/index.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const EntityParametersContainer = styled.div`
     width: 25%;
     background: var(--grey-975-75);
-    z-index: -10;
 `;
 
 const EntityParameters = () => {
