496389bdec10f8a887745a216c565a6906128742
ThomasDos <dosanjos.thomas@gmail.com>
2022-07-27 07:19:09 +0200

    feat: 🎸 landing/ ajout legende sur dropdown search

--- src/components/ui/LabelText/index.tsx
+++ src/components/ui/LabelText/index.tsx
@@ -5,26 +5,31 @@ interface LabelTextProps {
     text: string;
     backgroundColor?: string;
     icon?: string;
+    color?: string;
 }
 
 interface LabelTextContainerProps {
     backgroundColor: string;
+    color: string;
 }
 
 const LabelTextContainer = styled.div<LabelTextContainerProps>`
     border-radius: 40px;
     background: ${props => props.backgroundColor};
+    color: ${props => props.color};
 `;
 
 const LabelText = ({
     text,
     icon,
     backgroundColor = "#eeeeee",
+    color = "#000000",
 }: LabelTextProps) => {
     return (
         <LabelTextContainer
             className="py-1 px-4 flex justify-center items-center text-sm"
             backgroundColor={backgroundColor}
+            color={color}
         >
             {icon && (
                 <div>
