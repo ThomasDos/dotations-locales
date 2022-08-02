6f01241af85f77b24e96de8a29254c850901dc9d
ThomasDos <dosanjos.thomas@gmail.com>
2022-08-02 10:42:20 +0200

    style: 💄 dashboard entity/ update maquette

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,11 +1,15 @@
+import Image from "next/image";
 import styled from "styled-components";
 
 interface Buttonprops {
     text: string;
+    icon?: string;
 }
 
 const ButtonContainer = styled.button`
     width: 100%;
+    display: flex;
+    justify-content: center;
     background-color: var(--blue-france-113);
     padding: 12px 66px;
     color: var(--blue-france-975);
@@ -14,8 +18,21 @@ const ButtonContainer = styled.button`
         color: var(--blue-france-975);
     }
 `;
-const Button = ({ text }: Buttonprops) => {
-    return <ButtonContainer type="button">{text}</ButtonContainer>;
+const Button = ({ text, icon }: Buttonprops) => {
+    return (
+        <ButtonContainer type="button">
+            {icon && (
+                <Image
+                    src={`/icons/${icon}.svg`}
+                    width="20.8px"
+                    height="24px"
+                    layout="fixed"
+                    alt={`icone ${text}`}
+                />
+            )}
+            <span className={icon ? "ml-2" : ""}>{text}</span>
+        </ButtonContainer>
+    );
 };
 
 export default Button;
