bf41b555d5a9875307eb51f48b6679aaae5b0e39
ThomasDos <dosanjos.thomas@gmail.com>
2022-08-23 10:05:35 +0200

    feat: 🎸 installation et param redux persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import initialCommuneReducer from "./initialCommune.slice";
 import simulationCommuneReducer from "./simulationCommune.slice";
 
-const store = configureStore({
-    reducer: {
-        initialCommune: initialCommuneReducer,
-        simulationCommune: simulationCommuneReducer,
-    },
+const reducers = combineReducers({
+    initialCommune: initialCommuneReducer,
+    simulationCommune: simulationCommuneReducer,
 });
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+    reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
