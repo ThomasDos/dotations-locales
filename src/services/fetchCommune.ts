97190638046ba90aeaf61b8d011c102f16a5079b
Thomas DA <dosanjos.thomas@gmail.com>
2022-09-19 08:16:14 +0000

    feat: 🎸 general / retour maquette post simulation  & general

--- src/services/fetchCommune.ts
+++ src/services/fetchCommune.ts
@@ -2,12 +2,11 @@ import axios from "axios";
 import type { Commune, CommuneDto } from "src/models/commune/commune.interface";
 import { fetchCommuneSerializer } from "src/models/commune/commune.serializer";
 
-export default async (codeInsee: string): Promise<Commune> => {
-    return axios
+export default async (codeInsee: string): Promise<Commune> =>
+    axios
         .post(`${process.env.NEXT_PUBLIC_DOTATIONS_API_URL}/commune/`, {
             code_insee: codeInsee,
         })
         .then(({ data }: { data: CommuneDto }) => {
             return fetchCommuneSerializer(data);
         });
-};
