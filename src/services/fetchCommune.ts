449588f672e4486f9c098dfef1062c627d9fd251
Thomas DA <dosanjos.thomas@gmail.com>
2022-09-19 08:16:14 +0000

    feat: 🎸 general / retour maquette post simulation  & general

--- src/services/fetchCommune.ts
+++ src/services/fetchCommune.ts
@@ -1,14 +1,11 @@
 import axios from "axios";
 import type { Commune, CommuneDto } from "src/models/commune/commune.interface";
 import { fetchCommuneSerializer } from "src/models/commune/commune.serializer";
-import formatCodeInseeMetropole from "utils/formatCodeInseeMetropole";
 
 export default async (codeInsee: string): Promise<Commune> => {
-    const codeInseeFormatted = formatCodeInseeMetropole(codeInsee);
-
     return axios
         .post(`${process.env.NEXT_PUBLIC_DOTATIONS_API_URL}/commune/`, {
-            code_insee: codeInseeFormatted,
+            code_insee: codeInsee,
         })
         .then(({ data }: { data: CommuneDto }) => {
             return fetchCommuneSerializer(data);
