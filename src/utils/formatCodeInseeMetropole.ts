f8311eb33906e1d3484ab7a5a4e25e3eba3cffee
ThomasDos <dosanjos.thomas@gmail.com>
2022-09-28 17:34:31 +0200

    fix: 🐛 update tests

--- src/utils/formatCodeInseeMetropole.ts
+++ src/utils/formatCodeInseeMetropole.ts
@@ -1,13 +1,13 @@
 export default (codeInsee: string): string => {
-    const isMetropole = codeInsee.match(/^75|^13|^69/gm);
+    const isMetropole = codeInsee.match(/^(751|132|6938)/gm);
 
     if (isMetropole) {
         switch (isMetropole[0]) {
-            case "75":
+            case "751":
                 return "75056";
-            case "13":
+            case "132":
                 return "13055";
-            case "69":
+            case "6938":
                 return "69123";
             default:
                 return codeInsee;
